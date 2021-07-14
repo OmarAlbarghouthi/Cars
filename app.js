59a54f9c7b8a3eb6050555cf6f66e0b42201589b
OmarAlbarghouth <>
2021-07-14 12:40:54 +0000

    added some info

---
+++ app.js
@@ -0,0 +1,151 @@
+// syntax of js
+
+// /*
+// * data types
+// * numbers
+// ex 1 2 -2 0 4
+
+// * String = 'name' or "name"
+
+// * Boolean = true/false
+
+// */
+
+// /**
+//  * Variables = container for the values / storing
+//  * 
+//  * Var x = 12
+//  * 
+//  * var name = "Omar"
+//  * 
+//  */
+
+
+// // Operators
+// /**
+//  * 
+//  * assignment op ---> =
+//  * arthmatics op ---> + , - , * , / , %
+//  * 
+//  * comparasion op ---> 
+//  * 
+//  * > , < , == , === , != , >=
+//  */
+
+// var firstName = "Omar"
+// // show it on the console
+// // console.log(firstName)
+// // console.log("hello Omar")
+
+// // var x = "123"
+
+// // console.log(x)
+
+// // console.log(1+1 , 2>5 , 7<10 )
+
+// // var num = 10
+// // var num2 = 2021
+
+// // var concat = firstName + " " + num
+// // var sum = num + num2
+// // console.log(concat, "sum result" , sum)
+
+// // console.log(++num)
+
+// // // Boolean
+// // var isGreater = true
+
+
+// // var z 
+// // var y = z + 10
+
+// // console.log(z, "y" , y)
+
+// // console.log(1==1 , 1=="1" , 1=='10')
+
+// // console.log(1===1 , 1==='1')
+
+// /*
+// pop up boxes
+
+// alert("hey and welcome")// show info to the user
+
+// prompt() // input smth from user
+
+// document.write()
+
+// */
+
+// /*
+// logical Operators
+// && ! ||
+
+// truth table
+
+// T && T = true
+// T && F = false
+// T || T = true
+// T || F = true
+
+
+
+
+
+// */
+// // var name = prompt("whar is your name?")
+
+
+// // alert("hey"+" "+ name + " welcome to my Website")
+
+// // var favCar = prompt("what is your fav car?")
+
+// // console.log(name)
+
+// // document.write('<p>' + favCar + '<p>')
+
+// // document.write("hello from javascript")
+
+
+// // logical op ex
+
+// // console.log(1==1 && 1>0)
+// // console.log(1==9 && 1>0)
+// // console.log(1==1 || 1>0)
+
+// /*
+// conditional statment
+
+// if(condition){
+//   do smth
+// }
+// else{
+
+// }
+// */
+
+// // if(1<0 || 1===1){
+// //   console.log()
+
+// // }
+
+// var name = prompt("whar is your name?")
+
+
+// alert("hey"+" "+ name + " welcome to my Website")
+
+// var favCar = prompt("what is your fav car?")
+
+// if(favCar == "Lamborghini"){
+// document.write("<div>" + "<h3>" + favCar + "</h3> " + "<img" src ='https://robbreport.com/wp-content/uploads/2020/07/6-3.jpg' /> + "<div>")
+// }
+// else if (favCar == "Bugatti"){
+
+// }
+// else if(favCar == "Mercedes"){
+
+// }
+
+// else{
+//   alert("we don't have this car")
+// }
+
